d40273769ac7830a78df936cffeb05238db67d79
vaibhavsood <vaibhavs@us.ibm.com>
2019-01-24 16:15:46 +0530

    Update stackwalker_ppc64.cc

--- src/processor/stackwalker_ppc64.cc
+++ src/processor/stackwalker_ppc64.cc
@@ -83,7 +83,7 @@ StackFramePPC64* StackwalkerPPC64::GetCallerByCFIFrameInfo(
     "r8",  "r9",  "r10", "r11", "r12", "r13", "r14", "r15",
     "r16", "r17", "r18", "r19", "r20", "r21", "r22", "r23",
     "r24", "r25", "r26", "r27", "r28", "r29", "r30", "r31",
-    "srr0", "srr1", NULL
+    "srr0", "srr1", "lr", NULL
   };
 
   // Populate a dictionary with the valid register values in last_frame.
